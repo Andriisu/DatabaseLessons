c7186f9d0d84c38616548457c39c2a6c9c19615b
asuhorukov <54599056+asuhorukov@users.noreply.github.com>
2020-06-23 17:52:45 -0400

    Update SP_HomePro.sql

--- Lesson3/Solutions/SP_HomePro.sql
+++ Lesson3/Solutions/SP_HomePro.sql
@@ -1,6 +1,63 @@
+-- Task 1
+create procedure HomePro.GetAllCustomers_Andrey
+as
+begin
+	select CustomerId, FirstName, LastName, Email
+	from HomePro.Customers
+end
+go
+--Task 2
+create procedure HomePro.GetCustomersNoSchedule_Andrey
+as
+begin
+	select c.CustomerId, c.FirstName, c.LastName, c.Email, c.Phone, c.ZipCode, c.Age
+	from HomePro.Customers c
+		left join HomePro.Schedules s
+			on c.CustomerId = s.CustomerId
+	where s.CustomerId is null
+end
+go
+--task 3
+create procedure HomePro.GetCustomerByAge_Andrey
+	@age int 
+as
+begin
+	select * 
+	from HomePro.Customers
+	where Age > @age 
+end
+go
+--task 4
+create procedure HomePro.GetCustomerByAge_Andrey
+	@age int 
+as
+begin
+	if ( @Age<10 or @age> 100)
+	begin
+		raiserror('parameter Age is incorrect',16,10)
+		return 
+	end
+
+	select * 
+	from HomePro.Customers
+	where Age > @age 
+end
+go 
+--Task 5
+create procedure HomePro.GetCustomersNoScheduleByDate_Andrey
+	@DateNedeed datetime
+as
+begin
+	select c.CustomerId, c.FirstName, c.LastName, c.Email, c.Phone, c.ZipCode, c.Age
+	from HomePro.Customers c
+		left join HomePro.Schedules s
+			on c.CustomerId = s.CustomerId
+	where s.CustomerId is null
+		and s.DateNeeded = @DateNedeed
+end
+go
 
 --Task 6
-
 CREATE PROCEDURE HomePro.GetExtimationsWithPercentage_Andrey
 AS
 BEGIN 
