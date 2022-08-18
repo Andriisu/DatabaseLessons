38c7462bfc7a40a32d7cde913d7738d4d2077d8a
Andriisu <Andrew.s2712@gmail.com>
2022-08-18 15:04:21 -0400

    Restructure Homework

--- Lesson2/Solutions/Bank_Queries.sql
+++ Lesson2/Solutions/Bank_Queries.sql
@@ -1,102 +1,55 @@
---Schema HomePro
+--Aggregate Function
+--Table Bank.Clients
 --1
-select * 
-from HomePro.Customers c
-	join HomePro.Schedules s
-		on C.CustomerId = S.CustomerId 
-where s.DateNeeded > '2015-01-01'
-
+select min(Age), Avg(Age)
+from Bank.Clients
 --2
-select C.FirstName, C.LastName, C.Email, C.Phone, Q.Description, Q.Estimation
-from HomePro.Customers c
-	join HomePro.Quotes Q
-	on C.CustomerId = Q.CustomerId
-where Q.Estimation > 500
-order by Q.Estimation 
+select Avg(Age), count(*)
+from Bank.Clients 
+where Type = 'Private'
 --3
-select C.FirstName, C.LastName, C.Email, C.Phone, C.AltPhone, Q.ID as QuoteId, Q.Description, Q.Estimation
-from HomePro.Customers C
-	join HomePro.Quotes Q
-	on C.CustomerId = Q.CustomerId
-where C.AltPhone is NULL
-order by Q.Estimation
+select Max(Age) 
+from Bank.Clients
+where State != 'NJ'
+
+
+--Table Bank.Accounts
 --4
-select C.FirstName, C.LastName, C.Email, C.Phone, C.AltPhone, Q.ID as QuoteId, Q.Description, Q.Estimation
-from HomePro.Customers C
-	join HomePro.Quotes Q
-on C.CustomerId = Q.CustomerId
-	join HomePro.Schedules S
-on C.CustomerId = S.CustomerId
-where C.AltPhone is NULL and S.DateNeeded between '2016-01-01' and '2016-12-31'
-order by Estimation
+select Max(Balance) 'Biggest Balance' 
+from Bank.Accounts
 --5
-select * 
-from HomePro.Customers C
-	left join HomePro.Quotes Q
-	on C.CustomerId = Q.CustomerId
-where Q.CustomerId is null
+select ClientId, Avg(Balance) 'Average Balance' 
+from Bank.Accounts 
+group by ClientId
 --6
-select * 
-from HomePro.Customers C
-	left join HomePro.Quotes Q
-		on C.CustomerId = Q.CustomerId
-	left join HomePro.Schedules S
-		on C.CustomerId = S.CustomerId
-where Q.CustomerId is null and S.CustomerId is null
+select ClientId, Avg(Balance) 'Average Balance', Count(*) 'Number of Accounts' 
+from Bank.Accounts 
+group by ClientId
+
+--Table Bank.Transactions
 --7
-select * 
-from HomePro.Customers C
-	join HomePro.Schedules S
-		on C.CustomerId = S.CustomerId
-	left join HomePro.Quotes Q
-		on C.CustomerId = Q.CustomerId
-where Q.CustomerId is null 
+select Avg(Amount) as AverageNumberOfTransactions
+from Bank.Transactions
 --8
-select *
-from HomePro.Customers C
-	join HomePro.Quotes Q
-		on C.CustomerId = Q.CustomerId 
-	join HomePro.Schedules S
-		on C.CustomerId =  S.CustomerId
-where S.JobType ='Remodeling' and Q.Estimation < 5000 and C.Age < 70
---9
-select Sum(Q.Estimation) as SumOfQuotes
-from HomePro.Customers C
-	join HomePro.Quotes Q
-	on C.CustomerId = Q.CustomerId
-where C.NewsLetter = 1
---10
-select Sum(Q.Estimation) as SumOfQuotes
-from HomePro.Customers C
-	join HomePro.Quotes Q
-		on C.CustomerId = Q.CustomerId 
-	join HomePro.Schedules S
-		on C.CustomerId =  S.CustomerId
-where S.DateNeeded < '2015-01-01'
---11
-select count(*), Sum(Q.Estimation), AVG(Q.Estimation)
-from HomePro.Customers C
-	join HomePro.Quotes Q
-		on C.CustomerId = Q.CustomerId 
-	join HomePro.Schedules S
-		on C.CustomerId =  S.CustomerId
-where S.JobType ='Remodeling' and Q.Estimation < 5000 and C.Age < 70
+select AccountNumFrom, Count(*), Avg(Amount) as AverageNumberOfTransactions
+from Bank.Transactions
+group by AccountNumFrom
 
 
---Schema Bank
---12
+--Join Queries
+--1
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, A.Balance
 from Bank.Clients C
 	join Bank.Accounts A
 	on C.ClientId = A.ClientId 
 where A.Type = 'CHECKING'
---13
+--2
 select distinct C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, A.Balance, A.Type 
 from Bank.Clients C
 	join Bank.Accounts A
 	on C.ClientId = A.ClientId 
 where A.Type = 'CHECKING'or A.Type = 'credit'
---14
+--3
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, Acr.Balance, Acr.Type, Ach.Balance, Ach.Type 
 from Bank.Clients C
 	join Bank.Accounts Acr
@@ -105,13 +58,13 @@ from Bank.Clients C
 	on C.ClientId = Ach.ClientId 
 where Ach.Type = 'CHECKING' 
 	and Acr.Type = 'CREDIT'
---15
+--4
 select Sum(Balance) as 'Summary balance for private customers'
 from Bank.Clients C
 	join Bank.Accounts A
 	on C.ClientId = A.ClientId
 where C.Type = 'Private'
---16
+--5
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, T.Amount, T.Status, T.TransactionTime
 from Bank.Accounts A
 	join  Bank.Clients C
@@ -119,7 +72,7 @@ from Bank.Accounts A
 	join Bank.Transactions T
 		on A.AccountNum = T.AccountNumFrom
 where T.Status = 'pending'
---17
+--6
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, A.Balance, A.Type, T.Amount, T.Status, T.TransactionTime
 from Bank.Accounts A
 	join  Bank.Clients C
@@ -127,7 +80,7 @@ from Bank.Accounts A
 	join Bank.Transactions T
 	on A.AccountNum = T.AccountNumFrom
 where T.Status = 'rejected'
---18
+--7
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, A.Balance, A.Type, T.Amount, T.Status, T.TransactionTime
 from Bank.Accounts A
 	join  Bank.Clients C
@@ -135,7 +88,7 @@ from Bank.Accounts A
 	join Bank.Transactions T
 		on A.AccountNum = T.AccountNumFrom
 where T.TransactionTime > '2015-12-31'
---19
+--8
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, A.Balance, A.Type, T.Amount, T.Status, T.TransactionTime
 from Bank.Accounts A
 	join  Bank.Clients C
@@ -143,7 +96,7 @@ from Bank.Accounts A
 	join Bank.Transactions T
 		on A.AccountNum = T.AccountNumTo
 where A.Type = 'credit'
---20
+--9
 select C.FirstName, C.LastName, C.Phone, C.Email, C.State, C.Age, A.Balance, A.Type, T.TransactionId
 from Bank.Accounts A
 	join  Bank.Clients C
@@ -151,7 +104,7 @@ from Bank.Accounts A
 	left join Bank.Transactions T
 		on A.AccountNum = T.AccountNumFrom
 where T.TransactionId is NULL
---21
+--10
 select A.AccountNum, Count(T.AccountNumFrom) as 'Number of transactions', sum(T.Amount) as Sumarry
 from Bank.Accounts A
 	join  Bank.Clients C
@@ -160,7 +113,7 @@ from Bank.Accounts A
 		on A.AccountNum = T.AccountNumFrom
 where C.Age > 20
 group  by A.AccountNum
---22
+--11
 select Cfrom.FirstName as FirstNameFrom, Cfrom.LastName as LastNameFrom , Cto.FirstName as FirstNameTo, Cto.LastName as LastNameTo, T.Amount, T.Status, T.TransactionTime 
 from Bank.Transactions T
 	join  Bank.Accounts Afrom
